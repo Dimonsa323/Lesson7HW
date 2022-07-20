96e788626dbddad5fc5d254b5bf1f17d6f28516f
Dima Hubenya <dima96gubenya@gmail.com>
2022-07-20 11:37:23 +0300

    Initial Commit

---
+++ Lesson7HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson7HW
+//
+//  Created by Дима Губеня on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
